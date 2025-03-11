5ec322d67b80aae1c68f42cb44e46486f5657e63
Vyoman Jain <75789232+VyoJ@users.noreply.github.com>
2025-03-11 15:46:17 +0530

    smol change

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,6 +5,6 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run this despite the WiFi speed being 70 Kbps\n";
+  cout << "I have successfully built and run this with a change\n";
   return 0;
 }
